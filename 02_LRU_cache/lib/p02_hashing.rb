2ec68325b1d2a120243cc63359f36b6b08c72ef8
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-25 15:56:05 -0800

    finish LRU cache part 2

--- 02_LRU_cache/lib/p02_hashing.rb
+++ 02_LRU_cache/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    to_be_hashed = self.inject("") do |sum, el|
+      el = el.hash if el.is_a?(Array) || el.is_a?(Hash)
+
+      sum + (el.ord.to_s)
+    end
+
+    to_be_hashed.to_i ^ 101010
   end
 end
 
 class String
   def hash
+    to_be_hashed = self.chars.inject("") do |sum, el|
+      el = el.hash if el.is_a?(Array) || el.is_a?(Hash)
+
+      sum + (el.ord.to_s)
+    end
+
+    to_be_hashed.to_i ^ 101010
   end
 end
 
@@ -16,6 +30,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_be_hashed = self.keys + self.values
+    to_be_hashed = to_be_hashed.map(&:to_s).sort
+
+    to_be_hashed.hash
   end
 end
