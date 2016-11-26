a74cc9a901af4f9bef9f48568bb8d9d984f48be0
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-25 16:22:17 -0800

    finish part 4

--- 02_LRU_cache/lib/p04_linked_list.rb
+++ 02_LRU_cache/lib/p04_linked_list.rb
@@ -14,7 +14,13 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new(:head)
+    @tail = Link.new(:tail)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +29,68 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    link = find { |current| current.key == key }
+
+    link.nil? ? nil : link.val
   end
 
   def include?(key)
+    any? { |current| current.key == key }
   end
 
   def insert(key, val)
+    new_link = Link.new(key, val)
+
+    if empty?
+      new_link.prev = @head
+      @head.next = new_link
+
+      new_link.next = @tail
+      @tail.prev = new_link
+    elsif include?(key)
+      each { |link| link.val = val if link.key == key }
+    else
+      old_last = @tail.prev
+      new_link.prev = old_last
+      old_last.next = new_link
+
+      new_link.next = @tail
+      @tail.prev = new_link
+    end
   end
 
   def remove(key)
+    link = find { |current| current.key == key }
+    unless link.nil?
+      prev_link = link.prev
+      next_link = link.next
+
+      prev_link.next = next_link
+      next_link.prev = prev_link
+    end
   end
 
   def each
+    current = @head.next
+    until current == @tail
+      yield(current)
+      current = current.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
