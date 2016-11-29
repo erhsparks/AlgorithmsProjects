204ebd07c16665b63dce209541147ef434ab91ee
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-29 11:08:14 -0800

    finish heapsort

--- 03_heaps_heapsort/lib/heap_sort.rb
+++ 03_heaps_heapsort/lib/heap_sort.rb
@@ -2,5 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    prc = Proc.new { |el1, el2| el2 <=> el1 }
+
+    for i in (1...length) do
+      BinaryMinHeap.heapify_up(self, i, i + 1, &prc)
+    end
+
+    for i in (1...length) do
+      i = length - i
+      self[i], self[0] = self[0], self[i]
+      BinaryMinHeap.heapify_down(self, 0, i, &prc)
+    end
   end
 end
