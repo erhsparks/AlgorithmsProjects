f7a6ac1981fa547c84976e779b40f00eb3feedf5
Elizabeth Sparks <erhsparks@gmail.com>
2016-11-29 20:19:30 -0800

    finish quicksort

--- 04_quicksort/lib/quick_sort.rb
+++ 04_quicksort/lib/quick_sort.rb
@@ -25,8 +25,36 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    return if length <= 1
+
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    pivot_idx = QuickSort.partition(array, start, length, &prc)
+
+    QuickSort.sort2!(array, start, pivot_idx - start, &prc)
+    QuickSort.sort2!(array, pivot_idx, length - 1 - pivot_idx, &prc)
+    # seems like it would be more efficient to start at pivot_idx + 1, but the specs for worst case expect the extra comparsions of the pivot with itself...
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |a, b| a <=> b }
+
+    pivot_idx = start
+    pivot = array[pivot_idx]
+
+    (1...length).each do |i|
+      i += start
+      test_el = array[i]
+
+      if prc.call(test_el, pivot) == -1
+        array[pivot_idx] = test_el
+        pivot_idx += 1
+
+        array[i] = array[pivot_idx]
+        array[pivot_idx] = pivot
+      end
+    end
+
+    pivot_idx
   end
 end
